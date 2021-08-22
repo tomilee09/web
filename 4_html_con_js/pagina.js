5868bbfe07c2ea6db4ac55c8d401f2466d8b78db
Tomás Yáñez <toms25.y.c@gmail.com>
2021-08-22 00:59:41 -0400

    primer commit de mis archivos web

---
+++ 4_html_con_js/pagina.js
@@ -0,0 +1,9 @@
+console.log("Hola mundo, esta es la consola")
+
+// funcion pa implemetar en la página
+// cual es el área del círculo de radio r?:
+function areaCirculo()
+{
+    r = document.getElementById("input_html").value;
+    alert(r * r * Math.PI);
+}
